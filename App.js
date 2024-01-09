641d27756f0f6de2ed7bfe8289293aba11d40c70
abdulaziz <aboudecharkawi@gmail.com>
2024-01-09 13:02:18 +0200

    Intiallized and connected with mongoDB

---
+++ App.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import "dotenv/config";
+import connectDB from './config/MongoConfig.js';
+import cors from "cors"
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, (error) =>{ 
+    if(!error) {
+        console.log("Server is Running, and App is listening on port "+ PORT) 
+    } else {
+        console.log("Error: ", error)
+    }
+} 
+);
+connectDB()
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Monla-LLC
+
+# RealClientProject-Team4-BackEnd
+Team Members:
+- Rachwan Harb
+- Rayan Ali
+- Souhad Moussa
+- Abdulaziz Cherkawi
+- Abdulrahman Ghassa
