1d39bbc41b604bb25ab9d2e13d4b983209f5dadc
Rachwan <rachwan.harb2023@gmail.com>
2024-01-10 19:10:58 +0200

    Did the user models

--- Models/User.js
+++ Models/User.js
@@ -1,14 +1,31 @@
 import mongoose from "mongoose";
 
-
 const Schema = mongoose.Schema;
 
 const User = new Schema({
-name:{
-    type:String,
-    required:true
-}
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    number: {
+        type: String,
+    },
+    address: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+});
 
-})
-
-export default mongoose.model('User', User)
+export default mongoose.model("User", User);
