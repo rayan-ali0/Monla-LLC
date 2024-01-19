c7e03f9539ae4ff4be3b9e8cd20b2cb76c52f3b3
Rachwan <rachwan.harb2023@gmail.com>
2024-01-19 20:07:31 +0200

    Add it verifyToken and checkRole for the company/shipping routes

--- Routes/companyRoutes.js
+++ Routes/companyRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { companyController } from '../Controllers/CompanyController.js';
+import { verifyToken } from "../Middlewares/authentication.js";
+import { checkRole } from "../Middlewares/authentication.js";
 
 const companyRoutes = express.Router();
 
-companyRoutes.get('/', companyController.getCompany);
-companyRoutes.post('/create', companyController.createCompany);
-companyRoutes.put('/edit', companyController.editCompany);
-companyRoutes.delete('/delete', companyController.deleteCompany);
+companyRoutes.post('/create', verifyToken, checkRole(["admin"]), companyController.createCompany);
+companyRoutes.get('/', verifyToken, checkRole(["admin"]), companyController.getCompany);
+companyRoutes.put('/edit', verifyToken, checkRole(["admin"]), companyController.editCompany);
+companyRoutes.delete('/delete', verifyToken, checkRole(["admin"]), companyController.deleteCompany);
 
 export default companyRoutes;
--- Routes/shippingRoutes.js
+++ Routes/shippingRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { shippingController } from "../Controllers/ShippingController.js";
+import { verifyToken } from "../Middlewares/authentication.js";
+import { checkRole } from "../Middlewares/authentication.js";
 
 const shippingRoutes = express.Router();
 
-shippingRoutes.post("/create", shippingController.createShipping);
-shippingRoutes.get("/all", shippingController.getAllShippings);
-shippingRoutes.get("/:id", shippingController.getShippingById);
-shippingRoutes.put("/:id", shippingController.updateShipping);
-shippingRoutes.delete("/:id", shippingController.deleteShipping);
+shippingRoutes.post("/create", verifyToken, checkRole(["admin"]), shippingController.createShipping);
+shippingRoutes.get("/all", verifyToken, checkRole(["admin"]), shippingController.getAllShippings);
+shippingRoutes.get("/:id", verifyToken, checkRole(["admin"]), shippingController.getShippingById);
+shippingRoutes.put("/:id", verifyToken, checkRole(["admin"]), shippingController.updateShipping);
+shippingRoutes.delete("/:id", verifyToken, checkRole(["admin"]), shippingController.deleteShipping);
 
 export default shippingRoutes;
