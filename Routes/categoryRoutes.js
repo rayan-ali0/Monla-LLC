c1b3728793d5f7dcfef9b7483ab18bd2523ef407
abdulaziz <aboudecharkawi@gmail.com>
2024-01-29 11:17:02 +0200

    category

--- Routes/categoryRoutes.js
+++ Routes/categoryRoutes.js
@@ -1,13 +1,14 @@
 import  express  from "express";
 import { categoryController } from "../Controllers/CategoryController.js";
 import uploadImage from "../Middlewares/multer.js";
+import { verifyToken } from "../Middlewares/authentication.js";
 
 const categoryRouter=express.Router()
 
 categoryRouter.post("/addCategory", uploadImage.single("image"),categoryController.createCategory)
 categoryRouter.get("/readCategory", categoryController.getCategory)
 categoryRouter.get("/readCategoryById/:id", categoryController.getCategoryById)
-categoryRouter.put("/updateCategory", categoryController.updateCategory)
+categoryRouter.put("/updateCategory/:id", categoryController.updateCategory)
 categoryRouter.delete("/deleteCategory/:id", categoryController.deleteCategory)
 
 export default categoryRouter
